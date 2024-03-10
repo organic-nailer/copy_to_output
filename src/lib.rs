81c448915752b06f8fb63dd7c094137475b9ae51
organic-nailer <hykwyuk0125@gmail.com>
2024-03-10 20:44:45 +0900

    support workspace and library

--- src/lib.rs
+++ src/lib.rs
@@ -1,38 +1,11 @@
 use std::result::Result::Ok;
-use std::env;
 use fs_extra::copy_items;
 use fs_extra::dir::CopyOptions;
-use std::path::{Path, PathBuf};
+use std::path::Path;
 use anyhow::*;
 
 pub fn copy_to_output(path: &str, build_type: &str) -> Result<()> {
-    let mut out_path = PathBuf::new();
-    let mut cargo_target = String::new();
-    cargo_target.push_str("target");
-    match get_cargo_target_dir() {
-        Ok(target_dir) => cargo_target.push_str(target_dir.to_str().expect("Could not convert file path to string")),
-        Err(_) => (),
-    }
-
-    out_path.push(&cargo_target);
-
-    // This is a hack, ideally we would plug into https://docs.rs/cargo/latest/cargo/core/compiler/enum.CompileKind.html
-    // However, since the path follows predictable rules https://doc.rust-lang.org/cargo/guide/build-cache.html
-    // we can just check our parent path for the pattern target/{triple}/{profile}.
-    // If it is present, we know CompileKind::Target was used, otherwise CompileKind::Host was used.
-    // Best effort since the existing tests aren't intended to be run in a real build this won't exist.
-    // Unclear if that also means people in the wild are using the crate similarly, so avoiding any risk of break.
-    if let Ok(triple) = build_target::target_triple() {
-        if let Some(out_dir) = env::var_os("OUT_DIR") {
-            if let Some(out_dir) = out_dir.to_str() {
-                if out_dir.contains(&format!("{}{}{}", cargo_target, std::path::MAIN_SEPARATOR, triple)) {
-                    out_path.push(triple);
-                }
-            }
-        }
-    }
-
-    out_path.push(build_type);
+    let out_path = get_cargo_target_dir().expect("Could not get cargo target directory");
 
     // Overwrite existing files with same name
     let mut options = CopyOptions::new();
